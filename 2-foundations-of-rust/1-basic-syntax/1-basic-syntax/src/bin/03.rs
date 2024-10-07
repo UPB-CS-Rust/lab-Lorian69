df5e48b7d249a1af73c0c5af28e8fc6965caf44b
Lorian69 <rareshoprea@gmail.com>
2024-10-07 19:27:32 +0300

    Lab1

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,17 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let mut min = input[0];
+    let mut maxi = input[0];
 
-    println!("{} is largest and {} is smallest");
+    for x in input {
+        if x < min {
+            min = x;
+        }
+        if x > maxi {
+            maxi = x;
+        }
+    } 
+
+    println!("{} is largest and {} is smallest", maxi, min);
 }
